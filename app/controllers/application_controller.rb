596c199ca99b5acd958738c8a832cd6080c1ed56
fh <fhndiritu@gmail.com>
2025-02-25 21:48:17 +0300

    Style login page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
+  before_action :authenticate_user!
+
   before_action :set_active_storage_url_options
 
   after_action :render_flash_if_turbo
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get "receipts/show/:id" => "receipts#show", as: :show_receipt, defaults: { format: :pdf }
   get "receipts/print"
   resources :items
