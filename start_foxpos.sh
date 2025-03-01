1ba226352453c37dc728c5c2c09d5e576898c3b3
fh <fhndiritu@gmail.com>
2025-03-01 15:42:58 +0300

    Manually launch PWA from desktop scripts

--- start_foxpos.sh
+++ start_foxpos.sh
@@ -21,19 +21,21 @@ sudo docker-compose -f docker-compose.prod.yml down
 
 # Start Docker Compose using the production configuration
 echo "Starting Docker Compose..."
-sudo docker-compose -f docker-compose.prod.yml --env-file .env  up --build -d
+sudo docker-compose -f docker-compose.prod.yml --env-file .env  up --build
 
 # Wait for the application to start (adjust the sleep time if necessary)
 echo "Waiting for the application to start..."
 sleep 15
 
+
+# Manually launch the PWA
 # Debug: Check if the application is running
-echo "Checking if the application is running..."
-if curl --output /dev/null --silent --head --fail http://localhost:3000; then
-  echo "Application is running. Launching the PWA..."
-  # Launch the installed PWA
-  gtk-launch FoxPos
-else
-  echo "Application is not running. Please check the Docker logs."
-  exit 1
-fi
+# echo "Checking if the application is running..."
+# if curl --output /dev/null --silent --head --fail http://localhost:3000; then
+#   echo "Application is running. Launching the PWA..."
+#   # Launch the installed PWA
+#   gtk-launch FoxPos
+# else
+#   echo "Application is not running. Please check the Docker logs."
+#   exit 1
+# fi
