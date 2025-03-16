d68fd14a26d2d7ca15c76d8b32b44b9a98b21478
fh <fhndiritu@gmail.com>
2025-03-16 18:12:24 +0300

    User profile

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update destroy]
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
